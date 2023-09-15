5690d3f109c93b265e76d4d0a11cc77b98cf2ea6
MefAldemisov <alinnamef@gmail.com>
2023-09-15 18:46:53 +0300

    fix: one more price

--- src/assets/translations/de.ts
+++ src/assets/translations/de.ts
@@ -47,7 +47,7 @@ export const de = {
   },
   slots: {
     options: 'Sie können diesen Kurs mit folgenden Optionen kaufen',
-    optionsMeetingCard: 'Terminkarten ({n} Besuchen, €{price})',
+    optionsMeetingCard: 'Terminkarten ({n} Besuchen, {price}€)',
     optionsSubscriptions: '{n} Kurse pro Woche ({price}€)',
     fillInfo: 'Füllen Sie Ihre Informationen aus',
     selectTimeSlots: 'Wählen Sie die Zeitfenster aus, die Ihnen angenehm sind',
