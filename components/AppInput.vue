ee601650e5a3db9ab7f6c5f442420a1173fb5f2f
Shant <shant.ch.yotewo@gmail.com>
2024-09-06 05:07:15 -0700

    fix: add validation message to registration

--- components/AppInput.vue
+++ components/AppInput.vue
@@ -2,13 +2,16 @@
   <div v-if="type !== 'tel'" class="relative w-full">
     <input
       v-maska
-      class="w-full rounded-[12px] px-[24px] py-[16px] border-gray-200 border-[1px] text-[16px] relative"
-      :class="{
-        'bg-brand-light-gray': isGray && modelValue,
-        'bg-white disabled:bg-brand-light-gray': !isGray || !modelValue,
-      }"
+      :class="[
+        'w-full rounded-[12px] px-[24px] py-[16px] border-[1px] text-[16px]',
+        isInvalid ? 'border-red-500' : 'border-gray-200',
+        {
+          'bg-brand-light-gray': isGray && modelValue,
+          'bg-white disabled:bg-brand-light-gray': !isGray || !modelValue,
+        }
+      ]"
       :placeholder="placeholder"
-      :type="type ? (type === 'password' && isOpen ? 'text' : type) : 'text'"
+      :type="type ? (type === 'password' && isOpen ? 'text' : type) : type"
       :value="modelValue"
       maxlength="64"
       minlength="1"
@@ -45,7 +48,7 @@
     </div>
   </div>
   <div v-if="type === 'tel'" class="relative w-full">
-    <vue-tel-input
+    <VueTelInput
       class="w-full !rounded-[12px] !px-[24px] !py-[16px] !border-gray-200 !border-[1px] !text-[16px]"
       :class="{
         'bg-brand-light-gray': isGray && modelValue,
@@ -55,14 +58,15 @@
       :default-country="'us'"
       :preferred-countries="['us', 'gb', 'de', 'fr', 'in']"
       :input-options="{showDialCode: true}"
-      v-model="phone"
+      v-model="localPhone"
       @keydown="checkKeydown"
+      @blur="(event : any) => $emit('blur', event)"
     />
     
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch, defineProps, defineEmits, } from 'vue'
 import { VueTelInput } from 'vue-tel-input'
 import 'vue-tel-input/dist/vue-tel-input.css'
 
@@ -77,12 +81,22 @@ const props = defineProps<{
   pattern?: string
   max?: string
   min?: string
+  isInvalid: boolean
 }>()
 
-defineEmits(['update:modelValue', 'blur'])
+const emit = defineEmits(['update:modelValue', 'blur'])
 
 const isOpen = ref(false)
-const phone = ref('')
+const localPhone = ref(props.modelValue)
+
+watch(localPhone, (newValue) => {
+  emit('update:modelValue', newValue)
+})
+
+watch(() => props.modelValue, (newValue) => {
+  localPhone.value = newValue
+})
+
 
 const checkKeydown = (event: any) => {
   if (props.type === 'tel') {
@@ -90,8 +104,8 @@ const checkKeydown = (event: any) => {
       !event.ctrlKey &&
       !event.metaKey &&
       event.key.length === 1 &&
-      ((phone.value.length === 0 && event.key !== '+') ||
-        (phone.value.length !== 0 && /\D/.test(event.key)))
+      ((localPhone.value.length === 0 && event.key !== '+') ||
+        (localPhone.value.length !== 0 && /\D/.test(event.key)))
     ) {
       event.preventDefault()
     }
